a759ed2f8b83bee81ee6d4924137212d37764f93
Talha Bahabri <Talha.b.Future@gmail.com>
2022-06-22 06:11:16 +0300

    js api tests completed, change in css

--- scripts.js
+++ scripts.js
@@ -2,32 +2,24 @@ console.log(`this is a test`)
 
 
 
-getWeatherData()
-async function getWeatherData() {
-    // try {
-        const api_URL = `http://api.openweathermap.org/data/2.5/weather?q=Riyadh&APPID=4af83e00a0a923177f45e85a281f0556`
+getWeatherData("Riyadh")
+async function getWeatherData(location_Name) {
+    try {
+        const api_URL = `http://api.openweathermap.org/data/2.5/weather?q=${location_Name}&APPID=4af83e00a0a923177f45e85a281f0556`
         const response = await fetch(api_URL);
         const data =  await response.json();
         console.log(response)
         console.log(data)
         console.log(`jsonResponse specfiic data`) 
         console.log(data.main.temp)
+        console.log(data.name)
         
-    // }
-    // catch {
-    //     console.log(`error within the API`)
-    // }
+    }
+    catch {
+        console.log(`error within the API`)
+    }
   }
-
-  function setTimeoutPromise(delay) {
-    return new Promise((resolve, reject) => {
-      if (delay < 0) return reject("Delay must be greater than 0")
-  
-      setTimeout(() => {
-        resolve(`You waited ${delay} milliseconds`)
-      }, delay)
-    })
-  }
+ 
 
 // let  = `London`; 
 // async function getWeatherData(name) {
